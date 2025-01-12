6cb5888b43ca7ff50d3e6fbac8bcfcbbb66ab736
Reelback96 <114952726+Reelback96@users.noreply.github.com>
2025-01-11 20:23:45 -0500

    Update event-handlers.js

--- event-handlers.js
+++ event-handlers.js
@@ -3,4 +3,5 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("champion").addEventListener("click", voteChampion);
   document.getElementById("challenger").addEventListener("click", voteChallenger);
   document.getElementById("toggleSidebarBtn").addEventListener("click", toggleSidebar);
+  document.getElementById("hideSidebarBtn").addEventListener("click", toggleSidebar);
 });
